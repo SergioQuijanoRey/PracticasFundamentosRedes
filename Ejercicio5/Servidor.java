c04fd0798671667bf6fd6a6c10fe0bc0cf5cb9dc
Sergio Quijano Rey <sergiquijano@gmail.com>
2019-11-18 10:36:02 +0100

    Añado la estructura basica del proyecto Voy desarrollando algo el protocolo

---
+++ Ejercicio5/Servidor.java
@@ -0,0 +1,42 @@
+package Bingo;
+
+/**
+ * Clase que representa el servidor del juego del bingo
+ *
+ * @author Sergio Quijano Rey
+ * */
+public class Servidor{
+
+    // Atributos de la clase
+    //==========================================================================
+    int num_jugadores;  //> Indica cuantos jugadores se necesitan para jugar una partida
+
+    // Constructores
+    //==========================================================================
+
+    /**
+     * Constructor de la clase
+     * @param num_jugadores el numero de jugadores que se necesitan para jugar una partida
+     * */
+    Servidor(int num_jugadores){
+        this.num_jugadores = num_jugadores;
+    }
+
+    // Metodos publicos
+    //==========================================================================
+
+    // Metodos privados
+    //==========================================================================
+
+    // Ejecucion del programa
+    //==========================================================================
+
+    /**
+     * Metodo que ejecuta el codigo del servidor
+     * @param args los argumentos con los que se puede llamar al programa
+     * */
+    public void main(String[] args){
+
+    }
+
+}
