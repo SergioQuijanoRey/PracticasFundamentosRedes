2c22eb0cd04a321b25e5918774c9dbbe84f5cdcc
Sergio Quijano Rey <sergiquijano@gmail.com>
2019-11-18 11:46:28 +0100

    Creo dos clases auxiliares que creo que van a ser utiles

---
+++ Ejercicio5/Bingo.java
@@ -0,0 +1,13 @@
+package Bingo;
+
+/**
+ * Clase que representa el juego del bingo
+ *
+ * El proposito de esta clase es que vaya sacando numeros aleatorios entre un rango
+ * dado sin que estos numeros se repitan
+ *
+ * @author Sergio Quijano Rey
+ * */
+public class Bingo{
+
+}
---
+++ Ejercicio5/Codop.java
@@ -0,0 +1,11 @@
+package Bingo;
+
+/**
+ * Clase para que sea mas facil trabajar con los codigos de operacion enviados
+ * en los mensajes a traves de TCP
+ *
+ * @author Sergio Quijano Rey
+ * */
+public class Codop{
+
+}
