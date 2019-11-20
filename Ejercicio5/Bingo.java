68083f5a700ce16bb7cd15312eb8c16f5f04ab61
Sergio Quijano Rey <sergiquijano@gmail.com>
2019-11-20 18:12:28 +0100

    Implemento la clase bingo

--- Ejercicio5/Bingo.java
+++ Ejercicio5/Bingo.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Random;
 package Bingo;
 
 /**
@@ -9,5 +11,58 @@ package Bingo;
  * @author Sergio Quijano Rey
  * */
 public class Bingo{
+    // Atributos de la clase
+    //==========================================================================
+    ArrayList<int> bolas;
+    int num_bolas;
+    Random random;
+
+    // Constructores
+    //==========================================================================
+    /**
+     * Construye un bingo con un numero de bolas fijo
+     * @param num numero maximo de bolas
+     * */
+    Bingo(int num){
+
+        // Tomo el numero maximo de bolas
+        num_bolas = num;
+
+        // Uso el metodo privado
+        createBolas(num_bolas);
+
+        // Generador de numeros aleatorios
+        random = new Random();
+    }
+
+    /**
+     * Construye un bingo que por defecto tiene 100 numeros
+     * */
+    Bingo(){
+        this(100);
+    }
+
+    // Metodos
+    //==========================================================================
+    public void recreate(){
+        createBolas(num_bolas);
+    }
+
+    public int getBola(){
+        // Tomo un valor del array y lo saco
+        int index = random.nextInt();
+        int value = bolas.remove(index);
+
+        // Devuelvo el valor
+        return value;
+    }
+
+    // Metodos privados
+    //==========================================================================
+    private void createBolas(int num){
+        this.bolas = IntStream.rangeClosed(0, num).boxed().collect(Collectors.toArrayList());
+    }
+
+    // Testeo de la clase
 
 }
