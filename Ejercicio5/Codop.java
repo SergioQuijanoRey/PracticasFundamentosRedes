9ec2f6ff7447e7833ad14bcdef47b686f5557d2b
Sergio Quijano Rey <sergiquijano@gmail.com>
2019-11-20 17:53:02 +0100

    Implemento la clase codop

--- Ejercicio5/Codop.java
+++ Ejercicio5/Codop.java
@@ -8,43 +8,68 @@ package Bingo;
  * @author Sergio Quijano Rey
  * */
 public class Codop{
+    // Atributos de la clase
+    //==========================================================================
+    private int code;
+    private ArrayList<String> args;
+
     // Constructores
     //==========================================================================
     /**
      * Genera un codop a traves de un codigo y los argumentos
+     * @param code codigo del codop
+     * @param args argumentos del codop
      * */
     Codop(int code, ArrayList<String> args){
-
+        this.code = code;
+        this.args = args;
     }
 
     /**
      * Genera un codop a traves de un unico string de codop codificado que hay que formatear
+     * @param codop el string con el codop codificado
      * */
     Codop(String codop){
 
+        String[] partes = codop.split(",");
+        try{
+            this.code = Integer.parseInt(partes[0]);
+        } catch(Exception e){
+            System.out.println("No se ha podido parsear el codop");
+            System.err.println("Se genera codop vacio");
+        }
+
+        try{
+            this.args = partes[1].split(" ");
+        } catch(Exception e){
+            System.out.println("No se ha podido parsear los argumentos del codop");
+            System.err.println("Se genera codop vacio");
+        }
     }
 
     // Metodos 
     //==========================================================================
     /**
      * Obtiene el string del codop codificado
+     * @return el string codificado con el codigo y los argumentos
      * */
     public String getString(){
-
+        return String(this.code) + String(this.string);
     }
 
     /**
      * Obtiene el codigo del codop
+     * @return el codigo del codop
      * */
     public int getCode(){
-
+        return this.code;
     }
 
     /**
      * Obtiene la lista de argumentos del codop
+     * @return la lista con los argumentos
      * */
     public ArrayList<String> getArgs(){
-
+        return this.args;
     }
-
 }
