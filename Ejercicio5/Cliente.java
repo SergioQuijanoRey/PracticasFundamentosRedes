694dc9f071655164fda13c4d5586cececc2f9997
Sergio Quijano Rey <sergiquijano@gmail.com>
2019-11-20 18:46:28 +0100

    Empiezo a escribir la clase Cliente

--- Ejercicio5/Cliente.java
+++ Ejercicio5/Cliente.java
@@ -1,5 +1,60 @@
-package Bingo;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
-public class Cliente{
+/**
+ * Clase que representa un cliente del bingo
+ *
+ * @author Sergio Quijano Rey
+ * */
+public class Cliente extends Thread{
+    // Atributos de la clase
+    //==========================================================================
+    int port = 8989;
+    String host = "localhost";
+    int ID;
 
+    // Conexion al servidor
+    Socket servidor;
+    PrintWriter out;
+    BufferedReader in;
+
+    // Constructores
+    //==========================================================================
+    Cliente(){
+
+        // Conexion al servidor
+        connectToServer();
+
+        // Conseguimos los flujos de entrada salida
+        PrintWriter out = new PrintWriter(servidor.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(servidor.getInputStream()));
+        
+    }
+
+    // Metodos
+    //==========================================================================
+    // Metodos privados
+    //==========================================================================
+    private void connectToServer(){
+        try{
+            servidor = new Socket(host, port);
+        }catch(Exception e){
+            syserror("Error al intentar establecer la conexion con el servidor");
+        }
+    }
+
+    // Funcion principal
+    //==========================================================================
+    /**
+     * Funcion principal del cliente
+     * */
+    public static void main(String[] args){
+
+    }
 }
