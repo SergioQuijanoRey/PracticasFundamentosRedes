ba70c5df11d0850ed44c77633438e5229b98e5ea
Sergio Quijano Rey <sergiquijano@gmail.com>
2019-11-20 13:36:34 +0100

    Añado la estructura de la clase Codop

--- Ejercicio5/Codop.java
+++ Ejercicio5/Codop.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 package Bingo;
 
 /**
@@ -7,5 +8,43 @@ package Bingo;
  * @author Sergio Quijano Rey
  * */
 public class Codop{
+    // Constructores
+    //==========================================================================
+    /**
+     * Genera un codop a traves de un codigo y los argumentos
+     * */
+    Codop(int code, ArrayList<String> args){
+
+    }
+
+    /**
+     * Genera un codop a traves de un unico string de codop codificado que hay que formatear
+     * */
+    Codop(String codop){
+
+    }
+
+    // Metodos 
+    //==========================================================================
+    /**
+     * Obtiene el string del codop codificado
+     * */
+    public String getString(){
+
+    }
+
+    /**
+     * Obtiene el codigo del codop
+     * */
+    public int getCode(){
+
+    }
+
+    /**
+     * Obtiene la lista de argumentos del codop
+     * */
+    public ArrayList<String> getArgs(){
+
+    }
 
 }
