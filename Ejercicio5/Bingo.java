d0bb840bad71f6058c7e49ba73b8a7668b931393
Sergio Quijano Rey <sergiquijano@gmail.com>
2019-11-20 18:18:32 +0100

    Escribo el testeo del bingo

--- Ejercicio5/Bingo.java
+++ Ejercicio5/Bingo.java
@@ -44,10 +44,17 @@ public class Bingo{
 
     // Metodos
     //==========================================================================
+    /**
+     * Recreo el bingo con el numero de bolas especificado en el constructor
+     * */
     public void recreate(){
         createBolas(num_bolas);
     }
 
+    /**
+     * Se toma una bola del bingo al azar
+     * @return la bola del bingo elegida al azar
+     * */
     public int getBola(){
         // Tomo un valor del array y lo saco
         int index = random.nextInt();
@@ -59,10 +66,23 @@ public class Bingo{
 
     // Metodos privados
     //==========================================================================
+    /**
+     * Recrea el array que contiene las bolas [0, ..., num]
+     * @param num el maximo del conjunto
+     * */
     private void createBolas(int num){
         this.bolas = IntStream.rangeClosed(0, num).boxed().collect(Collectors.toArrayList());
     }
 
     // Testeo de la clase
+    //==========================================================================
+    public void main(String[] args){
+        System.out.println("Creando un bingo de pruebas");
+        Bingo pruebas = new Bingo(20);
 
+        System.out.println("Saco diez bolas del bingo");
+        for(int i = 0; i < 10; i++){
+            System.out.println("Valor sacado: " + bingo.getBola());
+        }
+    }
 }
